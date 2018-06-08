5991cea385957147e7e0de2abfdbd8fa952beff8
Michael Bach <migobach@gmail.com>
2018-06-07 19:12:05 -0600

    work adding cards

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
+// $(document).ready( function() {
+
+
 let current_card = ""
 
 const flashcards = [
   { question: "What is a string", answer: "less than 250 charachters" },
   { question: "Here is a question", answer: "Here is an answer."}
 ]
-let card = flashcards.length - 1
+
+
+
+function add_flashcard() {
+  var q = $('#front').val();
+  var a = $('#back').val();
+  flashcards.push( { question: q, answer: a} )
+}
+
 
 
 $('#newCard').on('click', function() {
+  // window.location.reload()
   current_card = flashcards[Math.floor(Math.random() * flashcards.length)]
   $('#showCard').text(current_card.question)
 })
@@ -21,39 +33,5 @@ $('#flipCard').click(function() {
 })
 
 
-function add_flashcard() {
-  var q = $('#front').val();
-  var a = $('#back').val();
-  flashcards.push( { q, a } )
-  alert(`Added at index ${card + 1}`);
-  card++;
 
 
-}
-
-
-
-
-
-
-// let x = 0;
-// const array = Array();
-
-// function add_element_to_array()
-// {
-//  array[x] = document.getElementById("text1").value;
-//  alert(`Element: ${array[x]} Added at index ${x}`);
-//  x++;
-//  document.getElementById("text1").value = "";
-// }
-
-// function display_array()
-// {
-//    let e = "<hr/>";   
-    
-//    for (let y=0; y<array.length; y++)
-//    {
-//      e += `Element ${y} = ${array[y]}<br/>`;
-//    }
-//    document.getElementById("Result").innerHTML = e;
-// }
