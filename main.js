1f28a1d8227f16d29bbe3684ca3e552678fa3763
Michael Bach <migobach@gmail.com>
2018-06-08 12:38:45 -0600

    add dropdown

--- main.js
+++ main.js
@@ -7,19 +7,22 @@ const flashcards = [
   { question: "What is a string", answer: "less than 250 charachters" },
   { question: "Here is a question", answer: "Here is an answer."}
 ]
-
-
+$.each(flashcards, function(index, flashcards) {
+  $('#card_options').append('<option>' + flashcards.question + '</option>');
+  $('#card_options2').append('<option>' + flashcards.answer + '</option>');
+});
 
 function add_flashcard() {
   var q = $('#front').val();
   var a = $('#back').val();
   flashcards.push( { question: q, answer: a} )
+  $('#card_options').append('<option>' + q + '</option>');
+  $('#card_options2').append('<option>' + a + '</option>');
 }
 
 
 
 $('#newCard').on('click', function() {
-  // window.location.reload()
   current_card = flashcards[Math.floor(Math.random() * flashcards.length)]
   $('#showCard').text(current_card.question)
 })
@@ -31,7 +34,18 @@ $('#flipCard').click(function() {
     $('#showCard').text(current_card.question)
   }
 })
+//below nee
 
+$('#deleteCard').click(function() {
+  // const index = Array.from(el.parentElement.chidren).indexOf(el)
+  // debugger
+})
+
+// onst index = Array.from(el.parentElement.children).indexOf(el)
+
+$('#editCard').click(function () {
+  //put in the edit feature
+})
 
 
 
